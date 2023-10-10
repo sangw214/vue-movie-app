f874fed778b9180294adc4b84f0b9dfed9031393
sangw214 <hybridspirit@gmail.com>
2023-10-10 11:28:17 +0900

    remove private info,,

--- src/store/about.js
+++ src/store/about.js
@@ -3,9 +3,9 @@ export default {
   /* 데이터 불변성 유지를 위해 function 으로 감싸서 리턴 */
   state: () => ({
     name: 'sangw',
-    email: 'hybridspirit@gmail.com',
+    email: 'gmail.com',
     blog: 'https://google.com',
-    phone: '+82-10-3311-2787',
+    phone: '+82-10-0000-0000',
     image: 'https://www.starbucks.co.kr/common/img/footer/recruit/icon_recruit01.png',
   })
 }
